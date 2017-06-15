ebab6173395a97e1dab6ed69e2169d2370a81bbc
happywanghao <654450587@qq.com>
2017-06-15 18:08:56 +0800

    2

--- src/components/AllFood.js
+++ src/components/AllFood.js
@@ -3,24 +3,42 @@ import {connect} from 'react-redux'
 import './css/allfood.css'
 import {getFootList,getNowRouter} from '../redux/actions/actions.js'
 class AllFood extends React.Component{
+  state={aa:1,showList:[]}
+  constructor(){
+    super()
+
+  }
   componentDidMount(){
     this.props.dispatch(getFootList(this.props.match.params.id))
-
     this.props.dispatch(getNowRouter(this.props.match.params.id))
   }
+  addNum(index){
+    let aa=this.state.showList
+    let bb=aa.map((it,i)=>(i===index ? {...it,num:it.num+1} : it))
+    this.setState({showList:bb})
+  }
+  componentWillReceiveProps(newProps){
+    if(newProps.foodList.length>0&&this.state.aa){
+      this.setState({showList:newProps.foodList.map(it=>({...it,num:0}))})
+      this.setState({aa:0})
+    }
+  }
   render(){
-    console.log(this.props.foodList);
     return (
       <div className='allfood'>
         <ul>
-          {this.props.foodList.map(item=>(
-            <li key={item._id}>
-              <img src={item.poster} alt='照片'/>
-              <span>{item.name}</span>
-              <span>&nbsp;&nbsp;单价：{item.price}</span>
-              <button>加入购物车</button>
-            </li>
-          ))}
+          {
+            this.state.showList.map((item,index)=>(
+              <li key={item._id}>
+                <img src={item.poster} alt='照片'/>
+                <span>{item.name}</span>
+                <span>&nbsp;&nbsp;单价：{item.price}</span>
+                <span> &nbsp;&nbsp;数量:</span>
+                <span>{item.num}</span>
+                <button onClick={this.addNum.bind(this,index)}>+</button>
+              </li>
+            ))
+          }
         </ul>
       </div>
     )
