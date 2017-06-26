2b06a0497cec14c53ae85656acfe5eae2ad6ed3c
happywanghao <654450587@qq.com>
2017-06-26 21:07:59 +0800

    2

--- src/components/Order.js
+++ src/components/Order.js
@@ -9,15 +9,8 @@ class Order extends React.Component{
   render(){
     return (
       <div className='order'>
-        dfs发生大幅度反倒是的所发生的
-        dfs发生大幅度反倒是的所发生的
-        dfs发生大幅度反倒是的所发生的
-        dfs发生大幅度反倒是的所发生的
-        dfs发生大幅度反倒是的所发生的
-        dfs发生大幅度反倒是的所发生的
-        dfs发生大幅度反倒是的所发生的
-        dfs发生大幅度反倒是的所发生的
-        
+    
+
       </div>
     )
   }
